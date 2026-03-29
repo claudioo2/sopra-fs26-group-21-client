8eb538782df5e029a312f0a8acc2fbfec3c58d7a
Claudio Carvalho <claudio.jp.carvalho@gmail.com>
2026-03-29 20:56:10 +0200

    remove import?

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useApi } from "@/hooks/useApi";
 import { EventDTO } from "@/types/event";
